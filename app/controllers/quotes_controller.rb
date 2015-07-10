87d57055285ff6d6ba970a357bc443885000de27
lucy-orbach <lucy.orbach@gmail.com>
2015-07-09 22:05:32 -0400

    routes and views added - TODO reactify!"

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,5 +2,31 @@ class QuotesController < ApplicationController
 	def index
 		@quotes = Quote.all
 	end
+
+	def new
+		@quote = Quote.new
+	end
+
+	def create
+		@quote = Quote.create(quote_params)
+		redirect_to @quote
+	end
+
+	def show
+		@quote = Quote.find(params[:id])
+	end
+
+	def destroy
+		quote = Quote.find(params[:id])
+    quote.destroy
+    redirect_to index
+  end
+
+	private
+    
+    def quote_params
+      params.require(:quote).permit( :movie, :character, :txt, :img)
+    end
+
 end
 
