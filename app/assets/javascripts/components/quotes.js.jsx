b748b11d725aac08468b9a116eab34fe99cbf620
lucy-orbach <lucy.orbach@gmail.com>
2015-07-10 00:23:56 -0400

    added jsx tutorial

--- app/assets/javascripts/components/quotes.js.jsx
+++ app/assets/javascripts/components/quotes.js.jsx
@@ -1,49 +1,151 @@
-// @Quotes = React.createClass
-//     getInitialState: ->
-//       quotes: @props.data
-//     getDefaultProps: ->
-//       quotes: []
-//     render: ->
-//        `<div className="quotes">
-//           <h2 className="title">Useful Quotes</h2>
-//         </div>
-
-//         `
-
-
- 
-var Quotes = React.createClass({displayName: 'Quotes',
-  render: function() {
+var Quote = React.createClass ({
+  render: function(){
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
     return (
-      React.createElement('div', {className: "quotes"},
-        <h1 className="title">Useful Quotes</h1>
-      )
-    );
-  }
-});
-React.render(
-  React.createElement(Quotes, null),
-  document.getElementById('content')
-);
-
-var QuotesList = React.createClass({
-  render: function() {
-    return (
-      <li className="quoteList">
-         <span><img src={this.props.img}/></span>
-         <h3>{this.props.txt}</h3>
-         <p>{this.props.character} - {this.props.movie}</p>
-       </li>
-    );
-  }
-});
-
-var QuotesForm = React.createClass({
-  render: function() {
-    return (
-      <div className="quotesForm">
-        Hello, world! I am a CommentForm.
+      <div className="quote">
+        <h2 className="quoteTxt">
+          {this.props.txt}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
 });
+
+var QuotesBox = React.createClass ({
+  loadQuotesFromServer: function(){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(){
+        this.setState({data: data});
+      }.bind(this),
+      error: function(){
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleQuoteSubmit: function(comment){
+    var quotes = this.state.data;
+    quotes.push(quote);
+    this.setState( {data: quotes}, function(){
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        type: 'POST',
+        data: quote,
+        success: function(data){
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function(){
+    this.loadQuotesFromServer();
+    setInterval(this.loadQuotesFromServer, this.props.pollInterval);
+  },
+  render: function(){
+    return (
+      <div className="quoteBox">
+        <h1>Useful Quotes</h1>
+        <QuoteList data={this.state.data} />
+        <QuoteForm onQuoteSubmit={this.handleQuoteSubmit} />
+      </div>
+    );
+  }
+});//QuotesBox
+
+var QuoteList = React.createClass ({
+  render: function(){
+    var quoteNodes = this.props.data.map( function(comment, index){
+      return (
+        <Quote txt={quote.txt} key={index}>
+          {quote.text}
+        </Quote>
+      );
+    });
+    return (
+      <div className="quoteList">
+        {quoteNodes}
+      </div>  
+    );
+  }
+});//QuotesLists
+
+var QuoteForm = React.createClass ({
+  handleSubmit: function(e){
+    e.preventDefault();
+    var txt = React.findDOMNode(this.refs.txt).value.trim();
+    var character = React.findDOMNode(this.refs.character).value.trim();
+    var movie = React.findDOMNode(this.refs.movie).value.trim();
+    var img = React.findDOMNode(this.refs.img).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onQuoteSubmit({txt: txt, character: character, movie: movie, img: img });
+    React.findDOMNode(this.refs.txt).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+  },
+  render: function(){
+    return (
+      <form className="quoteForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Enter Quote" ref="txt" />
+        <input type="text" placeholder="Enter Character" ref="character" />
+        <input type="text" placeholder="Enter Movie" ref="movie" />
+        <input type="text" placeholder="Enter Picture URL" ref="img" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+React.render(
+  <QuoteBox url="quotes.json" pollInterval={2000} />,
+  document.getElementById('content')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,10 @@
 class QuotesController < ApplicationController
 	def index
 		@quotes = Quote.all
+		respond_to do |format|
+  		format.html # index.html.erb
+  		format.json { render json: @quotes }
+		end
 	end
 
 	def new
