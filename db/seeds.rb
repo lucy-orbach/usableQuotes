60de193679b1d183c18d46032cc9e771c2294dbe
lucy-orbach <lucy.orbach@gmail.com>
2015-07-21 14:08:38 -0400

    seeds database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Quote.create(movie: 'Star Wars', character: 'Darth Vader', txt: "I'm your Father", img: 'http://www.starwarshelmets.com/septoct05/ANH_VADER_SCREENCAP_6.jpg')
+
+
+Quote.create(movie: "Star Wars", character: "Obi-Wan Kenobi", txt: "Use the Force, Luke!", img: "https://upload.wikimedia.org/wikipedia/en/3/32/Ben...")
+
+ 
+ Quote.create(movie: "Kung Fu Panda 2", character: "Po", txt: "Ah. My old enemy... stairs!", img: "https://veerendrasaraswathi.files.wordpress.com/20...")
+
+
+ Quote.create(movie: "Harry Potter", character: "Prof. Severus Snape", txt: "There will be no foolish wand-waving or silly inca...", img: "https://harpotzone.files.wordpress.com/2011/04/1146180704_apeprofile.jpg")
+ 
