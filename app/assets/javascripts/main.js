97d8b8f64ae38d76c0253dcf4d4492865e14fc88
lucy-orbach <lucy.orbach@gmail.com>
2015-07-22 18:35:10 -0400

    masonry file - not implemented

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,10 @@
+// $(function getMasonry(){
+// 	console.log("mason");
+// 	console.log($('.grid'));
+// 	$('.grid').masonry({
+// 	  itemSelector: '.grid-item',
+// 	  columnWidth: 270,
+// 	  isOriginTop: false 
+// 	});
+// });
+
